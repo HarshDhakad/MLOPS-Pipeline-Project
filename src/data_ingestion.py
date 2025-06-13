193db59637f2ac44273f6883f3f6d962ccdaa3c4
Harsh Dhakad <harshdhakad933@gmail.com>
2025-06-13 12:27:55 +0530

    After execute data_ingestion

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -71,7 +71,7 @@ def save_data(train_data: pd.DataFrame, test_data: pd.DataFrame, data_path: str)
 def main():
     try:
         test_size = 0.2
-        data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/main/spam.csv'
+        data_path = 'https://raw.githubusercontent.com/HarshDhakad/MLOPS-Pipeline-Project/refs/heads/main/files/spam.csv'
         df = load_data(data_url=data_path)
         final_df = preprocess_data(df)
         train_data, test_data = train_test_split(final_df, test_size=test_size, random_state=2)
